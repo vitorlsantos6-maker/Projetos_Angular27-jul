bd74517f370068514b8e10c372189d65c7b15b6d
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-07-27 19:52:39 -0300

    alterei o arquivo

---
+++ src/app/calculadora/calculadora.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Calculadora } from './calculadora';
+
+describe('Calculadora', () => {
+  let component: Calculadora;
+  let fixture: ComponentFixture<Calculadora>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Calculadora],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Calculadora);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
