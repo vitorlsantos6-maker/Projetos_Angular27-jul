12474fbd59208d539a112a4366cc9f7e9c6086c8
Vitor <vitor.l.santos6@aluno.senai.br>
2026-07-29 20:57:47 -0300

    fez de exemplo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Listaprodutos } from './listaprodutos/listaprodutos';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Listaprodutos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/calculadora/calculadora.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-calculadora',
-  imports: [],
-  templateUrl: './calculadora.html',
-  styleUrl: './calculadora.css',
-})
-export class Calculadora {}
